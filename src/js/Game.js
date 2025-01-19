79cdd2edd4ed82f516bd52800203131234ae075a
Anna Kuznetsova <ann_kuzn@mail.ru>
2025-01-19 21:56:08 +0500

    Homework No. 3 done

--- src/js/Game.js
+++ src/js/Game.js
@@ -7,6 +7,18 @@ export default class Game {
     this.previousIndex = null;
     this.currentIndex = null;
 
+    this.controller = document.querySelector(".controller");
+    this.cells = [...document.querySelectorAll(".cell")];
+    this.score = document.querySelector(".killed");
+    this.missed = document.querySelector(".missed");
+
+    this.previousScore = 0;
+    this.currentScore = 0;
+    this.missedCount = 0;
+
+    this.cells.forEach((cell) =>
+      cell.addEventListener("click", this.onCellClick.bind(this)),
+    );
     this.startBtn.addEventListener("click", this.onStartBtnClick.bind(this));
   }
 
@@ -15,11 +27,36 @@ export default class Game {
     this.start();
   }
 
+  onCellClick(event) {
+    if (event.target === this.goblin) {
+      this.goblin.remove();
+      this.currentScore += 1;
+      this.score.textContent = `Убил: ${this.currentScore}`;
+    }
+  }
+
   start() {
+    this.previousScore = 0;
+    this.currentScore = 0;
+    this.missedCount = 0;
+    this.score.textContent = "Убил: 0";
+    this.missed.textContent = "Промазал: 0";
+
     this.moveGoblin();
 
     this.intervalId = setInterval(() => {
       this.moveGoblin();
+
+      if (this.previousScore === this.currentScore) {
+        this.missedCount += 1;
+        this.missed.textContent = `Промахи: ${this.missedCount}`;
+        if (this.missedCount > 4) {
+          this.stop();
+          alert(`Game over! You killed ${this.currentScore}`);
+        }
+      } else {
+        this.previousScore = this.currentScore;
+      }
     }, 1000);
   }
 
